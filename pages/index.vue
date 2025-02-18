a9e5e76d94bc9591913f30d0729f79ee6ef7ac04
Yncy0 <paalayancy@gmail.com>
2025-02-18 20:51:28 +0800

    polish the button

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <template>
     <div class="flex flex-row min-w-full items-center justify-between">
 
-        <div class="flex flex-col gap-5 items-start">
-            <h1 class="text-[8rem] font-bold">
+        <div class="flex flex-col items-start">
+            <h1 class="text-[8rem] font-bold text-start">
                 Happy Monthsary!
             </h1>
-            <p>
+            <p class="pb-6">
                 I hope you’re doing well my love! Let me ask you a few questions on how we celebrate our monthsary.
             </p>
             <UButton 
                 label="Let's Get Started" 
                 size="xl" 
                 trailing-icon="i-lucide-arrow-right"
-                class="p-4"
+                class="p-4 bg-pink-500 hover:bg-pink-400"
                 :block="false"
             />
         </div>
 
         <img 
             src="/assets/svg/dummy_cat.svg"
-            class=" w-3xl"    
+            class=" w-3xl h-[640px]"    
         />
 
     </div>
