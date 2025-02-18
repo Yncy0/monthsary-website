c3a54ae1017aafc5898ba64cca524492cdad1c92
Yncy0 <paalayancy@gmail.com>
2025-02-18 15:14:32 +0800

    create header

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-w-full flex justify-center">
+  <div class="min-w-full flex flex-col justify-center">
+    <AppHeader/>
     <NuxtPage/>
   </div>
 </template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="min-w-full flex flex-row gap-3 bg-pink-500 p-7 text-white">
+        <a>Home</a>
+        <a>Memories</a>
+        <a>Albums</a>
+    </div>
+</template>
