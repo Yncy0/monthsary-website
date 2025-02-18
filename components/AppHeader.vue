598bc595acebaba68d33af155aaa84c7bdb13f81
Yncy0 <paalayancy@gmail.com>
2025-02-18 20:21:45 +0800

    migrate ordinary header into NavigationMenu by NuxtUI

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <!--Navigation Items list, scale the navigation here for futures references.
     For more infor: visit https://ui3.nuxt.dev/getting-started
 -->
-<script lang="ts">
+<script setup lang="ts">
     const items = ref([
         {
             label: 'Home',
@@ -22,12 +22,5 @@
     It uses NavigationMenu form NuxtUI, for more infor visit: https://ui3.nuxt.dev/components/navigation-menu
 -->
 <template>
-    <!-- <nav class="sticky top-0 z-50 shadow-2xl">
-        <ul class="min-w-full flex flex-row gap-3 bg-pink-500 p-7 text-white ">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/memories">Memories</NuxtLink></li>
-            <li><NuxtLink to="/albums">Albums</NuxtLink></li>
-        </ul>
-    </nav> -->
     <UNavigationMenu orientation="horizontal" :items="items" class="w-full justify-center bg-pink-500" />
 </template>
