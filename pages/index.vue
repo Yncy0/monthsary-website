7ba0d4ce22c9e595d664c77cfaab5def3ee44cb0
Yncy0 <paalayancy@gmail.com>
2025-02-19 15:01:47 +0800

    gallery section almost done

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col min-w-full justify-around items-center">
-        <section class="flex flex-row items-center justify-between">
+    <div class="flex flex-col min-w-full justify-center items-center">
+        <section class="flex flex-row justify-between h-[1024px] min-w-full">
             <div class="flex flex-col items-start">
                 <h1 class="text-[8rem] font-bold text-start">
                     Happy 
@@ -25,24 +25,41 @@
                 class=" w-3xl h-[640px]"    
             />
         </section>
+            
+        <section class="flex flex-col items-center h-[1024px] min-w-full">
             <h2 class="text-[5rem] font-bold text-pink-500">
                 Memories
             </h2>
             <p>
                 Here lies our precious memories that we did together, from the beginning until now. 
             </p>
-        <section>
-
         </section>
 
-        <section class="flex flex-row items-center justify-between">
-           <div class="flex flex-col items-start">
+        <section class="flex flex-row items-center justify-around min-w-full">
+           <div class="flex flex-col items-start w-[480px]">
                 <h3 class="text-[5rem] font-bold text-pink-500">
                     Gallery
                 </h3>
-                <p>
+                <p class="pb-6">
                     A collection of photos we took together. It contains serious, funny, silly, goofy, sweet, romantic photos with each other.
                 </p>
+                <UButton 
+                    label="View Gallery" 
+                    size="xl" 
+                    icon="i-mdi-image-multiple"
+                    class="p-4 shadow-2xl"
+                    :block="false"
+                />
+           </div>
+           <div class="flex flex-row ">
+            <img 
+                src="/assets/images/photo_booth2.jpg"
+                class="w-[400px] -rotate-12"    
+            />
+            <img 
+                src="/assets/images/photo_booth1.jpg"
+                class="w-[400px] rotate-12 top-28 relative"   
+            />
            </div>
         </section>
     </div>
