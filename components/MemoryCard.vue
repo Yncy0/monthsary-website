7f5ad0c9e04c2893c3c405cb3222a2c193e18b99
Yncy0 <paalayancy@gmail.com>
2025-02-19 18:49:50 +0800

    playing with props on components

---
+++ components/MemoryCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  headerImage: String,
+  description: String,
+});
+</script>
+
+<template>
+  <UCard>
+    <template #header>
+      <img :src="{ headerImage }" alt="images for card" />
+    </template>
+
+    <div>
+      <p>{{ description }}</p>
+    </div>
+
+    <template #footer> </template>
+  </UCard>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -33,6 +33,7 @@
           beginning until now.
         </p>
       </div>
+      <MemoryCard header-image="/assets/images/birb.jpg" description="Hello" />
       <div class="w-[1000px]">
         <AppCarousel />
       </div>
