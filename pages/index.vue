55fac826b57ea52ee516d1a5794b2f379d1b14b1
Yncy0 <paalayancy@gmail.com>
2025-02-26 18:33:22 +0800

    fix layout at first section element in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <template>
   <Transition :css="false">
     <div class="flex flex-col min-w-full justify-center items-center">
-      <section
-        class="container flex flex-row justify-between min-w-full h-[720px] pb-9"
-      >
+      <section class="flex flex-row justify-between min-w-full h-[720px] p-9">
         <div class="flex flex-col items-start">
           <h1
             class="text-[8rem] font-bold text-start dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,0.9)] drop-shadow-[0px_0px_39px_rgba(255,0,123,0.4)]"
@@ -27,18 +25,17 @@
             :block="false"
           />
         </div>
-        <img
-          src="/assets/images/Catto Copy.png"
-          width="684"
-          class="dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,0.5)] drop-shadow-[0px_0px_39px_rgba(0,255,30,0.9)]"
-        />
+        <div class="slime-catto w-[645px]">
+          <img
+            src="/assets/images/Catto Copy.png"
+            class="dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,0.5)] drop-shadow-[0px_0px_39px_rgba(0,255,30,0.9)]"
+          />
+        </div>
       </section>
 
       <USeparator />
 
-      <section
-        class="container flex flex-col min-w-full items-center gap-10 pb-20 pt-5"
-      >
+      <section class="flex flex-col min-w-full items-center gap-10 pb-20 pt-5">
         <div>
           <span class="dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,1)]">
             <h2
@@ -60,7 +57,7 @@
       <USeparator />
 
       <section
-        class="container flex flex-row items-center justify-around min-w-full h-[720px] pt-32"
+        class="flex flex-row items-center justify-around min-w-full h-[720px] pt-32"
       >
         <div class="flex flex-col items-start w-[480px]">
           <span class="dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,1)]">
