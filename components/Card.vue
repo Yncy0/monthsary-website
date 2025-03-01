00d8c64e042cb819bd16b0f781f757f2588e3782
Yncy0 <paalayancy@gmail.com>
2025-03-01 11:55:30 +0800

    visualize layoutin RoadmapIndex.vue

---
+++ components/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  header: String,
+  body: String,
+});
+</script>
+
+<template>
+  <UCard class="w-3xl">
+    <template #header>
+      <h1>{{ props.header }}</h1>
+    </template>
+    <div>
+      <p>{{ props.body }}</p>
+    </div>
+  </UCard>
+</template>
--- components/index/RoadmapIndex.vue
+++ components/index/RoadmapIndex.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-col items-center justify-center py-10">
+  <section class="flex flex-col items-center justify-center py-10 min-w-full">
     <span class="dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,1.0)]">
       <h3
         class="text-[5rem] font-bold text-pink-500 dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,0.9)] drop-shadow-[0px_0px_39px_rgba(255,0,123,0.3)]"
@@ -8,9 +8,35 @@
       </h3>
     </span>
     <p>Roadmap for our goals as a couple</p>
-    <div>
-      <div>
-        <img />
+    <div class="p-20 w-full flex flex-col gap-96">
+      <div class="flex flex-row justify-between">
+        <img src="/assets/images/gif/Catto(1) Copy.gif" class="w-[240px]" />
+        <Card header="Header 1" body="Lorem Ipsum" />
+      </div>
+
+      <div class="flex flex-row-reverse justify-between">
+        <img src="/assets/images/gif/Catto(1) Copy.gif" class="w-[240px]" />
+        <Card header="Header 1" body="Lorem Ipsum" />
+      </div>
+
+      <div class="flex flex-row justify-between">
+        <img src="/assets/images/gif/Catto(1) Copy.gif" class="w-[240px]" />
+        <Card header="Header 1" body="Lorem Ipsum" />
+      </div>
+
+      <div class="flex flex-row-reverse justify-between">
+        <img src="/assets/images/gif/Catto(1) Copy.gif" class="w-[240px]" />
+        <Card header="Header 1" body="Lorem Ipsum" />
+      </div>
+
+      <div class="flex flex-row justify-between">
+        <img src="/assets/images/gif/Catto(1) Copy.gif" class="w-[240px]" />
+        <Card header="Header 1" body="Lorem Ipsum" />
+      </div>
+
+      <div class="flex flex-row-reverse justify-between">
+        <img src="/assets/images/gif/Catto(1) Copy.gif" class="w-[240px]" />
+        <Card header="Header 1" body="Lorem Ipsum" />
       </div>
     </div>
   </section>
