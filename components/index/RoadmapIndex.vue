e979ee68f62d5565b2e63b020b5508b194746544
Yncy0 <paalayancy@gmail.com>
2025-03-01 11:36:33 +0800

    wip RoadmapIndex.vue

--- components/index/RoadmapIndex.vue
+++ components/index/RoadmapIndex.vue
@@ -1,6 +1,17 @@
 <template>
   <section class="flex flex-col items-center justify-center py-10">
-    <h1>Roadmap</h1>
+    <span class="dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,1.0)]">
+      <h3
+        class="text-[5rem] font-bold text-pink-500 dark:drop-shadow-[0px_0px_39px_rgba(255,0,123,0.9)] drop-shadow-[0px_0px_39px_rgba(255,0,123,0.3)]"
+      >
+        Roadmap
+      </h3>
+    </span>
     <p>Roadmap for our goals as a couple</p>
+    <div>
+      <div>
+        <img />
+      </div>
+    </div>
   </section>
 </template>
