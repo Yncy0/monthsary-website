7e7cdd3062a1484f80baae4cba992a087451d347
Yncy0 <paalayancy@gmail.com>
2025-03-01 21:08:06 +0800

    added RoadmapIndex.vue some of descriptions

--- components/Card.vue
+++ components/Card.vue
@@ -8,7 +8,13 @@ const props = defineProps({
 <template>
   <UCard class="w-2xl">
     <template #header>
-      <h1>{{ props.header }}</h1>
+      <span class="dark: drop-shadow-glow-pink-1000">
+        <h1
+          class="text-pink-500 text-[3rem] font-bold dark:drop-shadow-glow-pink-1000 drop-shadow-glow-pink-900"
+        >
+          {{ props.header }}
+        </h1>
+      </span>
     </template>
     <div>
       <p>{{ props.body }}</p>
--- components/index/RoadmapIndex.vue
+++ components/index/RoadmapIndex.vue
@@ -16,43 +16,43 @@ import RoadmapStack from "../RoadmapStack.vue";
       <RoadmapStack
         :isReversed="true"
         imgSource="/img/photobooth1.jpeg"
-        header="Header 1"
-        body="Lorem Ipsum"
+        header="Photobooth Date"
+        body="Photobooth image that started from SM MOA, the total number photobooth stall that we visited are: 4. 2 in SM MOA, 1 in SM San Fernando Pampanga, 1 in Robinson Place Malolos City. "
       />
 
       <RoadmapStack
         :isReversed="false"
         imgSource="/img/ice_cream.jpg"
-        header="Header 1"
-        body="Lorem Ipsum"
+        header="Ice Cream Date"
+        body="Always eating DQ with different flavors. You often didn't finished eating it!"
       />
 
       <RoadmapStack
         :isReversed="true"
         imgSource="/img/ice_skate.jpg"
-        header="Header 1"
-        body="Lorem Ipsum"
+        header="Ice Skating Date"
+        body="From 30 minutes of duration in the beginning, to max hours cap in the second visit. Such a worth it activity, I hope more of this to come!"
       />
 
       <RoadmapStack
         :isReversed="false"
         imgSource="/img/skyranch.jpeg"
-        header="Header 1"
-        body="Lorem Ipsum"
+        header="Skyranch San Fernando Pampanga"
+        body="Such a cute moment for our 1st monthsary celebration! Never forget the flowers you gave me."
       />
 
       <RoadmapStack
         :isReversed="true"
         imgSource="/img/coffee_date.jpg"
-        header="Header 1"
-        body="Lorem Ipsum"
+        header="Coffee Date"
+        body="You always invite me to drink coffee whenever you misses me, thanks Coffee Buddy!"
       />
 
       <RoadmapStack
         :isReversed="false"
         imgSource="/img/clay_date1.jpg"
-        header="Header 1"
-        body="Lorem Ipsum"
+        header="Clay Date"
+        body="One of the cutest moments in our relationship goals. From cutesy clays to something SPICY! OoooOOooo... decisions decision..."
       />
     </div>
   </section>
