acf0a11cab0c88795ef627fbad4e3f5fba450160
Yncy0 <paalayancy@gmail.com>
2025-03-01 20:54:03 +0800

    fix img height in RoadmapStack.vue

--- components/RoadmapStack.vue
+++ components/RoadmapStack.vue
@@ -11,7 +11,7 @@ const flexDir = ref(props.isReversed ? "row-reverse" : "row");
 
 <template>
   <div class="flex justify-between" :style="{ flexDirection: flexDir }">
-    <img :src="props.imgSource" class="w-[560px] h-[480px] rounded-lg" />
+    <img :src="props.imgSource" class="w-[560px] h-[560px] rounded-lg" />
     <Card :header="props.header" :body="props.body" />
   </div>
 </template>
