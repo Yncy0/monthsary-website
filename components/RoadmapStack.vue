c583156be2a23f1a7ac95ac09028735a7f034c58
Yncy0 <paalayancy@gmail.com>
2025-03-01 20:51:52 +0800

    Added RoadmapStack.vue to avoid unnecessary repetitive thing

---
+++ components/RoadmapStack.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  isReversed: Boolean,
+  imgSource: String,
+  header: String,
+  body: String,
+});
+
+const flexDir = ref(props.isReversed ? "row-reverse" : "row");
+</script>
+
+<template>
+  <div class="flex justify-between" :style="{ flexDirection: flexDir }">
+    <img :src="props.imgSource" class="w-[560px] h-[480px] rounded-lg" />
+    <Card :header="props.header" :body="props.body" />
+  </div>
+</template>
--- components/index/RoadmapIndex.vue
+++ components/index/RoadmapIndex.vue
@@ -1,3 +1,7 @@
+<script setup>
+import RoadmapStack from "../RoadmapStack.vue";
+</script>
+
 <template>
   <section class="flex flex-col items-center justify-center py-10 min-w-full">
     <span class="dark:drop-shadow-glow-pink-1000">
@@ -9,35 +13,47 @@
     </span>
     <p>Roadmap for our goals as a couple</p>
     <div class="p-20 w-full flex flex-col gap-72">
-      <div class="flex flex-row justify-between">
-        <img src="/img/photobooth1.jpeg" class="w-[560px] h-[480px]" />
-        <Card header="Header 1" body="Lorem Ipsum" />
-      </div>
+      <RoadmapStack
+        :isReversed="true"
+        imgSource="/img/photobooth1.jpeg"
+        header="Header 1"
+        body="Lorem Ipsum"
+      />
 
-      <div class="flex flex-row-reverse justify-between">
-        <img src="/img/ice_cream.jpg" class="w-[560px] h-[480px]" />
-        <Card header="Header 1" body="Lorem Ipsum" />
-      </div>
+      <RoadmapStack
+        :isReversed="false"
+        imgSource="/img/ice_cream.jpg"
+        header="Header 1"
+        body="Lorem Ipsum"
+      />
 
-      <div class="flex flex-row justify-between">
-        <img src="/img/ice_skate.jpg" class="w-[560px] h-[480px]" />
-        <Card header="Header 1" body="Lorem Ipsum" />
-      </div>
+      <RoadmapStack
+        :isReversed="true"
+        imgSource="/img/ice_skate.jpg"
+        header="Header 1"
+        body="Lorem Ipsum"
+      />
 
-      <div class="flex flex-row-reverse justify-between">
-        <img src="/img/skyranch.jpeg" class="w-[560px] h-[480px]" />
-        <Card header="Header 1" body="Lorem Ipsum" />
-      </div>
+      <RoadmapStack
+        :isReversed="false"
+        imgSource="/img/skyranch.jpeg"
+        header="Header 1"
+        body="Lorem Ipsum"
+      />
 
-      <div class="flex flex-row justify-between">
-        <img src="/img/coffee_date.jpg" class="w-[560px] h-[480px]" />
-        <Card header="Header 1" body="Lorem Ipsum" />
-      </div>
+      <RoadmapStack
+        :isReversed="true"
+        imgSource="/img/coffee_date.jpg"
+        header="Header 1"
+        body="Lorem Ipsum"
+      />
 
-      <div class="flex flex-row-reverse justify-between">
-        <img src="/img/clay_date1.jpg" class="w-[560px] h-[480px]" />
-        <Card header="Header 1" body="Lorem Ipsum" />
-      </div>
+      <RoadmapStack
+        :isReversed="false"
+        imgSource="/img/clay_date1.jpg"
+        header="Header 1"
+        body="Lorem Ipsum"
+      />
     </div>
   </section>
 </template>
