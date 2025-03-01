c0946d701fb4e0356c2a448ed0342d002f8188fd
Yncy0 <paalayancy@gmail.com>
2025-03-01 21:34:45 +0800

    simplified visuals in RoadmapStack.vue

--- components/RoadmapStack.vue
+++ components/RoadmapStack.vue
@@ -10,8 +10,18 @@ const flexDir = ref(props.isReversed ? "row-reverse" : "row");
 </script>
 
 <template>
-  <div class="flex justify-between" :style="{ flexDirection: flexDir }">
-    <img :src="props.imgSource" class="w-[560px] h-[560px] rounded-lg" />
-    <Card :header="props.header" :body="props.body" />
+  <div class="flex gap-36" :style="{ flexDirection: flexDir }">
+    <img :src="props.imgSource" class="w-[680px] h-[560px] rounded-lg" />
+    <!-- <Card :header="props.header" :body="props.body" /> -->
+    <div class="flex flex-col justify-center">
+      <span class="dark: drop-shadow-glow-pink-1000">
+        <h1
+          class="text-pink-500 text-[3rem] font-bold dark:drop-shadow-glow-pink-1000 drop-shadow-glow-pink-900"
+        >
+          {{ props.header }}
+        </h1>
+      </span>
+      <p>{{ props.body }}</p>
+    </div>
   </div>
 </template>
