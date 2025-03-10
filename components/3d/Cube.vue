2a7cf0eb1805f1f0f456a89e1942ad5cbb7d7074
Yncy0 <paalayancy@gmail.com>
2025-03-10 20:47:36 +0800

    playing with lighting in three js

--- components/3d/Cube.vue
+++ components/3d/Cube.vue
@@ -1,3 +1,5 @@
+<!--TODO: Refactor it I guess because it feels a spaghetti code-->
+
 <script setup>
 import * as THREE from "three";
 
@@ -26,26 +28,36 @@ onMounted(() => {
 
   //Init shapes aka GEOMETRY
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    wireframe: true,
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xff6347,
   });
   const cube = new THREE.Mesh(geometry, material);
+
+  cube.scale.x = 2.0;
+  cube.scale.y = 2.0;
+  cube.scale.z = 2.0;
+
   scene.add(cube);
 
+  //Lighting of the 3D Canvas
+  const pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(0.3, 2, 1);
+
+  const ambientLight = new THREE.AmbientLight(0xfffff);
+  scene.add(pointLight, ambientLight);
+
+  // const lightHelper = new THREE.PointLightHelper(pointLight);
+  // scene.add(lightHelper);
+
+  //Animations of the shape
   function animate() {
     requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    console.log("Being called");
 
     renderer.render(scene, camera);
   }
-
-  cube.scale.x = 2.0;
-  cube.scale.y = 2.0;
-  cube.scale.z = 2.0;
 
   animate();
 });
