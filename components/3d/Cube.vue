25477546f2cbb5b9fa47b6d9b054a340f5711d24
Yncy0 <paalayancy@gmail.com>
2025-03-10 15:22:49 +0800

    created Cube.vue component, trying to animate it

---
+++ components/3d/Cube.vue
@@ -0,0 +1,55 @@
+<script setup>
+import * as THREE from "three";
+
+//Make it run on Client only by putting onMounted()
+onMounted(() => {
+  //Init scene
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+
+  scene.add(camera);
+
+  const canvas = document.querySelector("#cube");
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(750, 500);
+
+  //Init shapes aka GEOMETRY
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true,
+  });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    console.log("Being called");
+
+    renderer.render(scene, camera);
+  }
+
+  cube.scale.x = 2.0;
+  cube.scale.y = 2.0;
+  cube.scale.z = 2.0;
+
+  animate();
+});
+</script>
+
+<template>
+  <canvas id="cube"></canvas>
+</template>
--- components/index/HeroIndex.vue
+++ components/index/HeroIndex.vue
@@ -1,40 +1,5 @@
 <script setup>
-//TODO: Put it on a separate file
-import * as THREE from "three";
-
-//Make it run on Client only by putting onMounted()
-onMounted(() => {
-  //Init scene
-  const scene = new THREE.Scene();
-
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.z = 5;
-
-  scene.add(camera);
-
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#three"),
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(500, 500);
-
-  //Init shapes aka GEOMETRY
-
-  const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    wireframe: true,
-  });
-  const torus = new THREE.Mesh(geometry, material);
-  scene.add(torus);
-
-  renderer.render(scene, camera);
-});
+import Cube from "../3d/Cube.vue";
 </script>
 
 <template>
@@ -64,12 +29,7 @@ onMounted(() => {
         :block="false"
       /> -->
     </div>
-    <canvas id="three"></canvas>
-    <!-- <div class="w-[645px]">
-        <img
-        src="/assets/images/gif/Catto(1) Copy.gif"
-        class="dark:drop-shadow-glow-pink-500"
-      />
-      </div> -->
+
+    <Cube />
   </section>
 </template>
