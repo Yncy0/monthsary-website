ee147fb80eb14852b1caa9d5c4ecdd0180361ba8
Yncy0 <paalayancy@gmail.com>
2025-03-21 21:46:10 +0800

    trying to create mini framework for ThreeJS, I guess

---
+++ components/three/ThreeCanvas.vue
@@ -0,0 +1,26 @@
+<script setup>
+import * as THREE from "three";
+
+const props = defineProps({
+  size: {
+    type: Array,
+    default: () => [0, 0], //width, height
+  },
+  alpha: Boolean,
+});
+
+const canvas = document.querySelector("#bg");
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: props.alpha,
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(props.size.at(0), props.size.at(1));
+</script>
+
+<template>
+  <canvas id="bg">
+    <slot />
+  </canvas>
+</template>
---
+++ components/three/ThreePerspectiveCamera.vue
@@ -0,0 +1,26 @@
+<script setup>
+import * as THREE from "three";
+
+const props = defineProps({
+  fov: Number,
+  aspect: Number,
+  near: Number,
+  far: Number,
+  position: {
+    type: Array,
+    default: () => [0, 0, 0], //x, y, z
+  },
+});
+
+const camera = new THREE.PerspectiveCamera(
+  props.fov,
+  props.aspect,
+  props.near,
+  props.far
+);
+camera.position.set(
+  props.position.at(0),
+  props.position.at(1),
+  props.position.at(2)
+);
+</script>
