b2e4bc23791898a8ce4c4739790729c712f3b572
Yncy0 <paalayancy@gmail.com>
2025-03-22 13:37:21 +0800

    added: TheeGLTFLoader.vue

---
+++ components/three/ThreeGLTFLoader.vue
@@ -0,0 +1,26 @@
+<script setup>
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const props = defineProps({
+  gltf: String,
+});
+
+const scene = new THREE.Scene();
+
+const loader = new GLTFLoader();
+
+loader.load(
+  props.gltf,
+
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+
+  undefined,
+
+  function (error) {
+    console.error(error);
+  }
+);
+</script>
