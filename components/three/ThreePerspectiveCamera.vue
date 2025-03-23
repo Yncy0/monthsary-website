3ea0afc56b601d0ee2f38fd54092b327f5b0ed34
Yncy0 <paalayancy@gmail.com>
2025-03-23 16:52:49 +0800

    trying to create the mini-wrapper for ThreeJS

---
+++ components/three/ThreePerspectiveCamera.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { PerspectiveCamera } from "three";
+
+const props = defineProps({
+  fov: Number,
+  aspect: {
+    type: Number,
+    default: () => window.innerWidth / window.innerHeight,
+  },
+  near: Number,
+  far: Number,
+  lookAt: {
+    type: Array,
+    default: () => [0, 0, 0], //[x: Number, y: Number: z: Number]
+  },
+  position: {
+    type: Array,
+    default: () => [0, 0, 0], //[x: Number, y: Number: z: Number]
+  },
+});
+
+//Client-side
+onMounted(() => {
+  const camera = new PerspectiveCamera(
+    props.fov,
+    props.aspect,
+    props.near,
+    props.far
+  );
+
+  camera.position.set(
+    props.position.at(0),
+    props.position.at(1),
+    props.position.at(2)
+  );
+
+  camera.lookAt(props.lookAt(0), props.lookAt(1), props.lookAt(2));
+});
+</script>
+
+<template></template>
