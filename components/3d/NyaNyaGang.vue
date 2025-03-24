4f28493bd390678e9d4bc3cd79b73bfbfa020b8e
Yncy0 <paalayancy@gmail.com>
2025-03-24 18:45:16 +0800

    added multiple gltf

--- components/3d/NyaNyaGang.vue
+++ components/3d/NyaNyaGang.vue
@@ -13,10 +13,10 @@ import {
 onMounted(() => {
   const scene = new THREE.Scene();
 
-  const camera = useThreePerspectiveCamera(75, undefined, 0.1, 1000, [0, 0, 5]);
+  const camera = useThreePerspectiveCamera(75, undefined, 0.1, 100, [0, 2, 5]);
 
   const canvas = document.querySelector("#bg") as HTMLCanvasElement;
-  const renderer = useThreeRender(undefined, canvas, undefined, [650, 500]);
+  const renderer = useThreeRender(false, canvas, undefined, [650, 500]);
 
   const ambientLight = useThreeAmbientLight(0xffffff);
   const pointLight = useThreePointLight(0xffffff, 10, 100, [3, 3, 3]);
@@ -25,13 +25,27 @@ onMounted(() => {
 
   const loader = new GLTFLoader();
 
-  loader.load(
-    "/nyanya/gangnam.glb",
+  loader.load("/nyanya/gangnam.glb", function (gltf) {
+    scene.add(gltf.scene);
+  });
 
-    function (gltf) {
-      scene.add(gltf.scene);
-    }
-  );
+  loader.load("/nyanya/capoiera.glb", function (gltf) {
+    scene.add(gltf.scene);
+
+    gltf.scene.position.set(4, 0, 0);
+  });
+
+  loader.load("/nyanya/hiphopdance.glb", function (gltf) {
+    scene.add(gltf.scene);
+
+    gltf.scene.position.set(-4, 0, 0);
+  });
+
+  loader.load("/nyanya/twerk.glb", function (gltf) {
+    scene.add(gltf.scene);
+
+    gltf.scene.position.set(-3, -2, 0);
+  });
 
   function animate() {
     requestAnimationFrame(animate);
