7495cf68836524573bc63016db0bc3b39eae9ab3
Yncy0 <paalayancy@gmail.com>
2025-03-30 00:53:15 +0800

    almost done on finishing 3D feature, cannot animate via TresJS might use ThreeJS in NyaNyaWorker.vue

---
+++ components/3d/NyaNyaWorker.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { LoopOnce } from "three";
+import { useAnimations, useGLTF } from "@tresjs/cientos";
+
+const { scene: model, animations } = await useGLTF("/nyanya/hammering.glb", {
+  draco: true,
+});
+
+model.position.set(0, -2.7, -6);
+
+// const actions = useAnimations(animations, model);
+// const currentAction = ref(actions["Armature|Armature|Smash"]);
+
+// currentAction.value.loop = LoopOnce;
+// currentAction.value.play();
+</script>
+
+<template>
+  <primitive :object="model" />
+</template>
--- components/wip/WIP.vue
+++ components/wip/WIP.vue
@@ -1,84 +1,14 @@
 <script setup lang="ts">
-import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
-
-import useThreePerspectiveCamera from "~/composables/three/useThreeCamera";
-import {
-  useThreeAmbientLight,
-  useThreePointLight,
-} from "~/composables/three/useThreeLight";
-import useThreeRender from "~/composables/three/useThreeRenderer";
-
-onMounted(() => {
-  const scene = new THREE.Scene();
-
-  const camera = useThreePerspectiveCamera(100, undefined, 0.1, 100, [0, 0, 5]);
-  camera.lookAt(0, 0, 0);
-
-  const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-  const renderer = useThreeRender(true, canvas, undefined, [
-    window.innerWidth,
-    window.innerHeight,
-  ]);
-
-  const pointLight = useThreePointLight(0xffffff, 100, 100, [0, 0, 10]);
-
-  const ambientLight = useThreeAmbientLight(0xffffff, 2);
-
-  scene.add(camera, pointLight, ambientLight);
-
-  const loader = new GLTFLoader();
-  loader.load(
-    "/nyanya/hammering.glb",
-
-    function (gltf) {
-      scene.add(gltf.scene);
-
-      const mixer = new THREE.AnimationMixer(gltf.scene);
-      const clips = gltf.animations;
-
-      gltf.scene.scale.setScalar(0.7);
-      gltf.scene.position.set(-5, -2.5, -1);
-      gltf.scene.rotateY(45.5);
-
-      const clock = new THREE.Clock();
-
-      function update() {
-        mixer.update(clock.getDelta());
-      }
-
-      function animate() {
-        requestAnimationFrame(animate);
-        update;
-      }
-
-      animate();
-
-      const clip = THREE.AnimationClip.findByName(
-        clips,
-        "Armature|mixamo.com|Layer0"
-      );
-      const action = mixer.clipAction(clip);
-      action.play();
-    },
-
-    undefined,
-
-    function (error) {
-      console.error(error);
-    }
-  );
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    renderer.render(scene, camera);
-  }
-
-  animate();
-});
+import NyaNyaWorker from "~/components/3D/NyaNyaWorker.vue";
 </script>
 
 <template>
-  <canvas class="fixed top-0 left-0" id="canvas" />
+  <TresCanvas alpha window-size>
+    <TresPerspectiveCamera :fov="75" :position="[0, 0, 5]" />
+    <TresAmbientLight :intensity="2" />
+    <TresPointLight :intensity="100" :distance="100" :position="[0, 0, 10]" />
+    <Suspense>
+      <NyaNyaWorker />
+    </Suspense>
+  </TresCanvas>
 </template>
