d19b70d0f2bc28bade6c61f949d2ba752d55ce66
Yncy0 <paalayancy@gmail.com>
2025-06-04 14:51:17 +0800

    finished: populate 3d mesh based on the numbers of items in an array (via ref() due to data's reactivity later)

--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const items = ref([]);
+const mock = getMockRoadmap();
 
 const scene = new THREE.Scene();
 
 function addMesh() {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x0ff00 });
+  const material = new THREE.MeshBasicMaterial({ color: 0x0ff000 });
   const box = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
   box.position.set(x, y, z);
 
   scene.add(box);
 }
 
-Array(200).fill().forEach(addMesh)
-
 onMounted(() => {
   items.value = getMockRoadmap();
+
+  items.value.forEach(addMesh)
+
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -30,18 +35,27 @@ onMounted(() => {
 
   camera.position.z = 5;
 
-  scene.add(camera);
+  const light = new THREE.AmbientLight(0x000000);
+
+  scene.add(camera, light);
 
   const canvas = document.querySelector("#bg") as HTMLCanvas;
-  const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+  const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  function animate(){
-    renderer.render(scene, camera)
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  controls.update();
+
+  function animate() {
+    renderer.render(scene, camera);
+
+    controls.update();
   }
 
-  renderer.setAnimationLoop(animate)
+  renderer.setAnimationLoop(animate);
 });
+
 </script>
 
 <template>
